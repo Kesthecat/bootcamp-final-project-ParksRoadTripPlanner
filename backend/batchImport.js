495bbc1a22258ceed8c829b93e5e925d4eff4ab7
Gina Wong <twgwong@gmail.com>
2022-04-07 14:40:24 -0400

    batch import of data to mongodb done

---
+++ backend/batchImport.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// const alberta = require("./data/Alberta.json");
+// const bc = require("./data/BC.json");
+// const canada = require("./data/Canada.json");
+// const manitoba = require("./data/Manitoba.json");
+// const nl = require("./data/NL.json");
+// const nova = require("./data/Nova Scotia.json");
+// const ontario = require("./data/Ontario.json");
+// const pei = require("./data/PEI.json");
+// const saskatchewan = require("./data/Saskatchewan.json");
+// const sepaq = require("./data/Sepaq.json");
+
+const batchImport = async () => {
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("planner");
+    const result = await db.collection("parks").insertOne(sepaq);
+  } catch (err) {
+    console.log("There was an error: ", err.message);
+  }
+  client.close();
+};
+
+// batchImport();
