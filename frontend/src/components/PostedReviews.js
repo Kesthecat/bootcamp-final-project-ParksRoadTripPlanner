9637d8292231b665018d36cf348b0700f85d603f
Gina Wong <twgwong@gmail.com>
2022-04-11 00:25:55 -0400

    park page structure done

---
+++ frontend/src/components/PostedReviews.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PostedReviews = ({ id }) => {
+  return (
+    <ReviewsWrapper>
+      <p>Review 1</p>
+      <p>Review 2</p>
+      <p>Review 3</p>
+    </ReviewsWrapper>
+  );
+};
+
+const ReviewsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
