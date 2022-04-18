a74fe14766f631f33cd44241e1bd82835f925438
Gina Wong <twgwong@gmail.com>
2022-04-18 01:38:28 -0400

    trip page styled

--- frontend/src/components/trips/Trip.js
+++ frontend/src/components/trips/Trip.js
@@ -17,7 +17,6 @@ export const Trip = () => {
   const [trip, setTrip] = useState(null);
   const [hasStops, setHasStops] = useState(false);
   const [legsInfo, setLegsInfo] = useState([]);
-  // const [last, setLast] = useState(0);
 
   const {
     setMap,
@@ -61,13 +60,11 @@ export const Trip = () => {
         if (data.data.waypoints.length > 0) {
           setHasStops(true);
           setWaypoints(data.data.waypoints);
-          // setLast(data.data.routeMetrics.length - 1);
         }
         setTrip(data.data);
       })
       .catch((error) => {
         console.error("error", error);
-        // window.alert(error.message);
         history.push("/Error");
       });
   }, []);
@@ -82,8 +79,6 @@ export const Trip = () => {
     legsInfo.reduce((acc, cur) => acc + cur.distance.value, 0) / 100;
   // console.log("distance", sumDistance);
 
-  // setTotalDistance(sumDistance); ///get Too many re-render error message
-
   const durationSumSec = legsInfo.reduce(
     (acc, cur) => acc + cur.duration.value,
     0
@@ -97,67 +92,90 @@ export const Trip = () => {
   // console.log({ departure, legsInfo });
 
   return (
-    <Container>
+    <>
       <TripName>{trip.tripName}</TripName>
-      <InfoContainer>
-        <Wrapper className="Route">
-          <InfoWrapper>
-            <StyledP>Departure: </StyledP>
-            <StyledP>{trip.departure.name}</StyledP>
-            <StyledP>Distance: {legsInfo[0].distance.text}</StyledP>
-            <StyledP>Duration: {legsInfo[0].duration.text}</StyledP>
-          </InfoWrapper>
-          {hasStops &&
-            waypoints.map((waypoint, i) => {
-              return (
-                <InfoWrapper key={waypoint._id}>
-                  <ParkLink to={`/parks/${waypoint._id}`}>
-                    {waypoint.name}
-                  </ParkLink>
-                  <StyledP>Distance: {legsInfo[i + 1].distance.text}</StyledP>
-                  <StyledP>Durating: {legsInfo[i + 1].duration.text}</StyledP>
-                </InfoWrapper>
-              );
-            })}
-          <InfoWrapper>
-            <StyledP>Destination: </StyledP>
-            <StyledP>{trip.destination.name}</StyledP>
-            {/* {!hasStops && (
-              <>
-                <StyledP>Distance: {legsInfo[last].distance.text}</StyledP>
-                <StyledP>Durating: {legsInfo[last].duration.text}</StyledP>
-              </>
-            )} */}
-          </InfoWrapper>
-        </Wrapper>
-        <Wrapper className="Driving">
-          <InfoWrapper>
-            <StyledP>Total Distance: </StyledP>
-            <StyledP>{sumDistance} km</StyledP>
-          </InfoWrapper>
-          <InfoWrapper>
-            <StyledP>Driving duration: </StyledP>
-            <DurationWrapper>
-              {durationObj.days !== 0 && (
-                <StyledP>{durationObj.days} days(s)</StyledP>
-              )}
-              {durationObj.hours !== 0 && (
-                <StyledP>{durationObj.hours} hour(s)</StyledP>
-              )}
-              {durationObj.minutes !== 0 && (
-                <StyledP>{durationObj.minutes} minutes</StyledP>
-              )}
-            </DurationWrapper>
-          </InfoWrapper>
-        </Wrapper>
-        <Wrapper className="Buttons">
-          <StyledBtn>Edit trip btn</StyledBtn>
-          <StyledBtn>Delete trip btn</StyledBtn>
-          <StyledBtn>Share trip btn</StyledBtn>
-        </Wrapper>
-      </InfoContainer>
-      <MapContainer>
-        {/* <GoogleMapReact
+      <Container>
+        <InfoContainer>
+          <Wrapper className="trip">
+            <InfoWrapper className="departDesti">
+              <StyledP>
+                <StyledSpan>Departure: </StyledSpan>
+                {trip.departure.name}
+              </StyledP>
+            </InfoWrapper>
+            <InfoWrapper className="stopInfo">
+              <DistanceDuration>
+                Driving Distance: {legsInfo[0].distance.text}
+              </DistanceDuration>
+              <DistanceDuration>
+                Driving Time: {legsInfo[0].duration.text}
+              </DistanceDuration>
+            </InfoWrapper>
+            <Wrapper className="waypoints">
+              {hasStops &&
+                waypoints.map((waypoint, i) => {
+                  return (
+                    <Wrapper key={waypoint._id}>
+                      <InfoWrapper
+                        style={{
+                          width: "350px",
+                          backgroundColor: "var(--color-secondary)",
+                        }}
+                      >
+                        <StyledSpan>Stop {i + 1}: </StyledSpan>
+                        <ParkLink to={`/parks/${waypoint._id}`}>
+                          {waypoint.name}
+                        </ParkLink>
+                      </InfoWrapper>
+                      <InfoWrapper className="stopInfo">
+                        <DistanceDuration>
+                          Driving Distance: {legsInfo[i + 1].distance.text}
+                        </DistanceDuration>
+                        <DistanceDuration>
+                          Driving Time: {legsInfo[i + 1].duration.text}
+                        </DistanceDuration>
+                      </InfoWrapper>
+                    </Wrapper>
+                  );
+                })}
+            </Wrapper>
+            <InfoWrapper className="departDesti">
+              <StyledP>
+                <StyledSpan>Destination: </StyledSpan>
+                {trip.destination.name}
+              </StyledP>
+            </InfoWrapper>
+          </Wrapper>
+          <Wrapper className="driving">
+            <StyledP>
+              <StyledSpan>Total Distance: </StyledSpan>
+              {sumDistance} km
+            </StyledP>
+            <InfoWrapper className="drivingTime">
+              <StyledP>
+                <StyledSpan>Driving duration: </StyledSpan>
+              </StyledP>
+              <DurationWrapper>
+                {durationObj.days !== 0 && (
+                  <StyledP>{durationObj.days} days(s)</StyledP>
+                )}
+                {durationObj.hours !== 0 && (
+                  <StyledP>{durationObj.hours} hour(s)</StyledP>
+                )}
+                {durationObj.minutes !== 0 && (
+                  <StyledP>{durationObj.minutes} minutes</StyledP>
+                )}
+              </DurationWrapper>
+            </InfoWrapper>
+          </Wrapper>
+          <Wrapper className="buttons">
+            <StyledBtn>Edit</StyledBtn>
+            <StyledBtn>Delete</StyledBtn>
+            <StyledBtn>Share</StyledBtn>
+          </Wrapper>
+        </InfoContainer>
+        <MapContainer>
+          {/* <GoogleMapReact
           bootstrapURLKeys={bootstrapURLKeys}
           defaultCenter={{ lat: 51.90994, lng: -100.50986 }}
           defaultZoom={4}
@@ -166,51 +184,109 @@ export const Trip = () => {
         >
           {!!departure && (
             <MdLocationPin
-              size={40}
-              lat={departure.coordinates.lat}
-              lng={departure.coordinates.lng}
+            size={40}
+            lat={departure.coordinates.lat}
+            lng={departure.coordinates.lng}
             />
-          )}
-          {!!destination && (
-            <MdLocationPin
+            )}
+            {!!destination && (
+              <MdLocationPin
               size={40}
               lat={destination.coordinates.lat}
               lng={destination.coordinates.lng}
-            />
-          )}
-          {waypoints.map((waypoint, i) => {
-            return (
+              />
+              )}
+              {waypoints.map((waypoint, i) => {
+                return (
               <LocationMarker
-                key={i}
-                lat={waypoint.coordinates.lat}
+              key={i}
+              lat={waypoint.coordinates.lat}
                 lng={waypoint.coordinates.lng}
                 park={waypoint}
-              />
+                />
             );
           })}
         </GoogleMapReact> */}
-      </MapContainer>
-    </Container>
+        </MapContainer>
+      </Container>
+    </>
   );
 };
 
-const Container = styled.div``;
-const TripName = styled.h1``;
+const TripName = styled.h1`
+  margin-bottom: 15px;
+  border-bottom: 1px solid var(--color-main);
+  padding-bottom: 10px;
+`;
+const Container = styled.div`
+  display: flex;
+`;
+const StyledSpan = styled.span`
+  font-weight: bold;
+`;
 const InfoContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 15px 25px;
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  &.trip {
+    padding: 15px;
+    background-color: var(--color-main);
+  }
+  &.driving {
+    width: fit-content;
+    padding: 15px;
+    margin-left: 50px;
+  }
+  &.buttons {
+    flex-direction: row;
+    justify-content: space-between;
+    border-top: 2px solid var(--color-secondary);
+    padding-top: 15px;
+  }
+`;
 const InfoWrapper = styled.div`
   display: flex;
-  border: 2px solid purple;
+  flex-direction: column;
+  gap: 10px;
+  margin: 5px 0;
+  /* border: 2px solid purple; */
+  padding: 10px;
+
+  &.departDesti {
+    width: 350px;
+    background-color: var(--color-secondary);
+  }
+  &.stopInfo {
+    margin-left: 50px;
+    font-size: 20px;
+    background-color: var(--color-tertiary);
+  }
+  &.drivingTime {
+    gap: 0px;
+    padding: 0;
+  }
 `;
+const DistanceDuration = styled.p``;
 const StyledP = styled.p``;
 const ParkLink = styled(NavLink)``;
-const StyledBtn = styled.button``;
+const StyledBtn = styled.button`
+  height: 45px;
+  font-size: 20px;
+  padding: 10px;
+`;
 const MapContainer = styled.div`
   height: 600px;
-  width: 1000px;
+  width: 820px;
+  margin-top: 15px;
+  /* border: 2px solid blue; */
 `;
 const DurationWrapper = styled.div`
-  display: flex;
+  padding-top: 5px;
+  padding-left: 162px;
+  margin-top: -24px;
 `;
