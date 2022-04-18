b932958d4af682bb350050d70ec39f8bf437d93c
Gina Wong <twgwong@gmail.com>
2022-04-18 15:13:15 -0400

    error and coming soon page styled

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -1,13 +1,23 @@
+import styled from "styled-components";
 import lost from "../assets/map-what.gif";
 
 export const ErrorPage = () => {
   return (
-    <>
-      <img src={lost} alt="lost looking at map" />
-      <p>
-        An error has occured. Try again or contact customer services if the
-        error persists.
-      </p>
-    </>
+    <Container>
+      <StyledImg src={lost} alt="lost looking at map" />
+      <h3>An error has occured.</h3>
+      <h3>Try again or contact customer services if the error persists.</h3>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
+const StyledImg = styled.img`
+  width: 400px;
+`;
