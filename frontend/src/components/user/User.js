f55a9b30d7d13725c16416bc90cd50c8876e26fa
Gina Wong <twgwong@gmail.com>
2022-04-26 15:47:09 -0400

    added gifs and screenshoot for ReadMe

--- frontend/src/components/user/User.js
+++ frontend/src/components/user/User.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, useHistory, useParams } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 
